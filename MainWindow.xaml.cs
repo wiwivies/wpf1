0bcf2ad04a39ef4180678adf76655327238094be
wivivies <vikaberezhna1@gmail.com>
2023-11-15 00:22:00 +0200

    ex 2 was done

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace wpf1
 {
     /// <summary>
@@ -20,9 +21,130 @@ namespace wpf1
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool isAnsw = false;
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void ButtonNumberClick(object sender, RoutedEventArgs e)
+        {
+            char buttonContent = (sender as Button).Content.ToString()[0];
+
+            if (char.IsDigit(buttonContent) || buttonContent == ',')
+            {
+                if (isAnsw)
+                {
+                    TBBottom.Text = "";
+                    TBTop.Text = "";
+                }
+            }
+
+            if (buttonContent == ',')
+            {
+                if (TBBottom.Text.Length == 0 || !char.IsDigit(TBBottom.Text[TBBottom.Text.Length - 1]))
+                {
+                    TBBottom.Text += '0';
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            TBBottom.Text += buttonContent;
+            isAnsw = false;
+
+            if (TBBottom.Text.Length > 1 && TBBottom.Text[TBBottom.Text.Length - 2] == '0' && TBBottom.Text[TBBottom.Text.Length - 1] != ',')
+            { 
+            
+            }
+            else if (TBBottom.Text.Length > 2 && TBBottom.Text[TBBottom.Text.Length - 3] == '0' && (TBBottom.Text[TBBottom.Text.Length - 1] < '0' || TBBottom.Text[TBBottom.Text.Length - 1] > '9'))
+            { 
+            
+            }
+            else
+            {
+                return;
+            }
+
+            MessageBox.Show("Incorrect input!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            TBBottom.Text = "";
+        }
+
+        private void ButtonEqualClick(object sender, RoutedEventArgs e)
+        {
+            if (TBBottom.Text.Length == 0)
+            {
+                MessageBox.Show("Expression is empty!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            TBTop.Text = TBBottom.Text;
+
+            List<double> numbers = new List<double>();
+            string txt = "";
+            char prev = '0';
+
+            foreach (var item in TBBottom.Text + "/")
+            {
+                if (char.IsDigit(item) || item == ',')
+                {
+                    txt += item;
+                    continue;
+                }
+                else if (prev == '*')
+                {
+                    numbers[numbers.Count - 1] *= Convert.ToDouble(txt);
+                }
+                else if (prev == '/')
+                {
+                    numbers[numbers.Count - 1] /= Convert.ToDouble(txt);
+                }
+                else if (prev == '-')
+                {
+                    numbers.Add(-Convert.ToDouble(txt));
+                }
+                else if (prev == '+')
+                {
+                    numbers.Add(Convert.ToDouble(txt));
+                }
+                else
+                {
+                    try
+                    {
+                        numbers.Add(Convert.ToDouble(txt));
+                    }
+                    catch
+                    { }
+                }
+
+                if (item == '+' || item == '-' || item == '*' || item == '/')
+                {
+                    prev = item;
+                }
+                txt = "";
+            }
+
+            TBBottom.Text = Math.Round(numbers.Sum(x => x), 8).ToString();
+            isAnsw = true;
+        }
+
+        private void ButtonBackClick(object sender, RoutedEventArgs e)
+        {
+            if (TBBottom.Text != "")
+                TBBottom.Text = TBBottom.Text.Remove(TBBottom.Text.Length - 1);
+        }
+        private void ButtonCEClick(object sender, RoutedEventArgs e)
+        {
+            TBBottom.Text = "";
+        }
+
+        private void ButtonCClick(object sender, RoutedEventArgs e)
+        {
+            TBBottom.Text = "";
+            TBTop.Text = "";
+        }
+
     }
 }
